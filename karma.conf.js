4c3f3069fc625024f4f9a333ba3f1a047e199341
vitogit <apuntalorapido@gmail.com>
2016-02-24 14:54:05 -0300

    First TDD iteration: it exists a hello tag

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
     ],
     files: [
       'node_modules/chai/chai',
-      'test/**/*.tag',
+      'src/**/*.tag',
       'test/**/*.js'
     ],
     preprocessors: {
--- test/helo-spec.js
+++ test/helo-spec.js
@@ -1,3 +1,8 @@
 describe('Hello world spec', function() {
-
+  it('exists a hello tag', function() {
+    var html = document.createElement('hello')
+    document.body.appendChild(html)
+    tag = riot.mount('hello')[0]
+    expect(tag).to.be.ok
+  })
 })
