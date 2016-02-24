bee1236b36ae673d31f527ee95f50c8d04dde4b0
vitogit <apuntalorapido@gmail.com>
2016-02-24 15:27:02 -0300

    Second TDD iteration: has a name property

--- src/hello.tag
+++ src/hello.tag
@@ -1,2 +1,6 @@
 <hello>
+
+  <script>
+    this.name = ''
+  </script>
 </hello>
--- test/helo-spec.js
+++ test/helo-spec.js
@@ -1,8 +1,16 @@
 describe('Hello world spec', function() {
-  it('exists a hello tag', function() {
+  it('mounts a hello tag', function() {
     var html = document.createElement('hello')
     document.body.appendChild(html)
     tag = riot.mount('hello')[0]
-    expect(tag).to.be.ok
+    expect(tag).to.exist
+    expect(tag.isMounted).to.be.true    
   })
+  
+  it('has a name property ', function() {
+    var html = document.createElement('hello')
+    document.body.appendChild(html)
+    tag = riot.mount('hello')[0]
+    expect(tag.name).to.exist
+  })  
 })
